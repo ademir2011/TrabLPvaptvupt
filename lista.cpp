664b165cde7ada516b2909d9145d25fbe24c8591
Leonardo <leoeutropio@gmail.com>
2014-10-20 16:05:22 -0300

    jhbgyh

---
+++ lista.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lista.h"
+
+
+void CriarCaixa();
+void InserirNaFila();
+void RemoverDaFila();
+void AdicionarCaixa();
+void RetirarCaixa();
--- lista.h
+++ lista.h
@@ -1,4 +1,23 @@
 #ifndef _LISTA_H_
 #define _LISTA_H_
 
+struct Lista
+{
+	int pessoasnafila;
+	struct Lista *prox;
+};
+
+class Fila{
+	struct Lista *inicio;
+	struct Lista *fim;
+};
+
+
+void CriarCaixa();
+void InserirNaFila();
+void RemoverDaFila();
+void AdicionarCaixa();
+void RetirarCaixa();
+
+
 #endif
